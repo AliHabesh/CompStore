b94d83682a07123d2f3af2addb374607bb9235b1
AliHabesh <ali.habesh0068@stud.hkr.se>
2023-01-18 11:45:38 +0100

    added new features such as transfering money, working, buying and retrieving computer images

--- ComputerStoreLogic.js
+++ ComputerStoreLogic.js
@@ -1,5 +1,61 @@
 let selectList = document.getElementById("list");
+selectList.addEventListener("click", displayLaptopInformation);
+let laptopInfoParagraph = document.getElementById('laptop-info');
+let computerImage = document.getElementById('compImage');
+let computerTitle = document.getElementById('computerTitle');
+let largeComputerDescription = document.getElementById('largeComputerDescription');
+let computerPrice = document.getElementById('computerPrice');
+let workButton = document.getElementById('work');
+let salary = document.getElementById('salary');
+let transferButton = document.getElementById('transfer');
+let workAmount = 0; 
+let balance = document.getElementById('currentBalance'); 
+let loanButton = document.getElementById('getLoan'); 
+let alreadyLoanedMoney = false;
+let currentDebt = 0;
 
+
+
+//Computer store logic --------------------------------------------
+workButton.addEventListener("click", work);
+transferButton.addEventListener("click", transferWorMoney);
+loanButton.addEventListener("click", takeLoan);
+
+function transferWorMoney(){
+  let money = 0;
+  if(currentDebt > 0){
+    money = Math.abs(currentDebt - Number(salary.innerHTML));
+    currentDebt = Math.abs(currentDebt - Number(salary.innerHTML));
+    alert('Your current debt after transfering money: '+Math.abs(currentDebt))
+  }else{
+    money = Number(salary.innerHTML);
+  }
+  balance.innerHTML = Number(balance.innerHTML) + Number(money);
+  salary.innerHTML = Number(0);
+}
+
+function work(){
+  console.log(Number(salary.value))
+  salary.innerHTML = Number(salary.innerHTML) + Number(100);
+}
+
+function takeLoan(){
+  let loanAmount = prompt('How much would you like to loan?')
+  if(currentDebt > 0){
+    alert('Sorry, you cant loan more than once!'+"\n"+'You have to pay back the current loan you have which is at: ')
+    return;
+  }
+  if(!(Number(loanAmount) > Number(balance.innerHTML)*2)){
+    balance.innerHTML = Number(balance.innerHTML)+Number(loanAmount);
+    currentDebt = Number(loanAmount);
+  }else{
+    alert('You can not take a loan that is more than two times larger than your current balance!');
+  }
+}
+
+
+//API data retrieval -----------------------------------------------------------
+let list;
 function fillList(list) {
   for (var i = 0; i < list.length; i++) {
     var option = document.createElement("option");
@@ -8,18 +64,53 @@ function fillList(list) {
     selectList.appendChild(option);
   }
 }
-
-//API data retrieval
-
-let list;
 async function fetchData() {
   const response = await fetch(
     "https://hickory-quilled-actress.glitch.me/computers"
   );
   list = await response.json();
+  
   fillList(list);
   return list;
 }
+fetchData();
 
-let lists = await fetchData();
-console.log(lists);
+async function displayLaptopInformation(){
+  var e = document.getElementById("list");
+  var value = e.value;
+  var text = e.options[e.selectedIndex].text;
+  console.log(text);
+  findLaptopInfoBasedOnName(text);
+
+}
+
+async function findLaptopInfoBasedOnName(laptopName){
+  const response = await fetch(
+    "https://hickory-quilled-actress.glitch.me/computers"
+  );
+  list = await response.json();
+
+  for(let i = 0; i < list.length; i++){
+    if(list[i].title == laptopName){
+      laptopInfoParagraph.innerText = list[i].specs[0] + "\n"+list[i].specs[1];
+      computerImage.src = 'https://hickory-quilled-actress.glitch.me/'+list[i].image;
+      computerTitle.innerText = list[i].title;
+      computerPrice.innerText = list[i].price;
+      largeComputerDescription.innerText = list[i].description;
+    }
+  }
+}
+
+async function fetchComputerImage(url, compName){
+  const response = await fetch(
+    "https://hickory-quilled-actress.glitch.me/computers"
+  );
+  list = await response.json();
+  for(let i = 0; i < list.length; i++){
+    if(list[i].title == compName){
+      computerImage.src = url;
+    }
+  }
+  computerImage.src = url;
+}
+
